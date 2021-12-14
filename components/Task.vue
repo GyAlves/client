6cc7b685f2df0666b9d0ae26391160d3eb0a05b5
GyAlves <gyasminassuncao@uni9.edu.br>
2021-12-14 20:04:24 -0300

    Finished todos dashboard

--- components/Task.vue
+++ components/Task.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="task_container">
-    <div class="done_btn_container"></div>
-    <div class="task_description_container">
-      <div class="description_container">
-        <h2>Learn VueJs</h2>
-        <span>Learn vueJs using youtube videos</span>
-      </div>
-      <div class="priority_label_container">
-        <p>Low</p>
-      </div>
+    <div class="complete_task_btn"></div>
+    <div class="task_description">
+      <h3>Learn VueJs</h3>
+      <span>Freecodecamp youtube videos</span>
     </div>
+    <div class="delete_task_btn">X</div>
   </div>
 </template>
 
@@ -21,62 +17,59 @@ export default {
 
 <style>
 .task_container {
-  width: 75%;
-  height: 20%;
-  border-radius: 10px;
-  padding-top: 1em;
-  border: 2px solid #3b335577;
+  position: absolute;
+  width: 290px;
+  height: 72.51px;
+  left: 47px;
+  top: 231px;
 
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  align-items: flex-start;
-}
-
-.task_container .done_btn_container {
-  width: 25px;
-  height: 25px;
-  border-radius: 50%;
-  border: 3px solid #5d5d81;
-}
-
-.task_container .task_description_container {
-  width: 85%;
-  height: 90%;
-
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: flex-start;
-}
-
-.task_container .task_description_container .description_container {
-  height: 87%;
-  width: 90%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-
-.task_container .task_description_container .description_container h2 {
-  font-size: 1.2em;
-  color: #5d5d81;
-}
-
-.task_container .task_description_container .description_container span {
-  color: #5d5d816c;
-}
-
-.task_container .task_description_container .priority_label_container {
-  width: 17%;
+  background: #8bd7d2;
+  border: 4px solid #00bd9d5e;
   border-radius: 10px;
 
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-around;
   align-items: center;
+}
 
-  background-color: #69abe8;
-  color: #ffffff;
+.task_container .complete_task_btn {
+  width: 25px;
+  height: 25.53px;
+  border-radius: 50%;
+  border: 3px solid #00bd9d;
+
+  background-color: #fffbfa;
+}
+
+.task_container .task_description {
+  width: 65%;
+  height: 70%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+}
+
+.task_container .task_description h3 {
+  font-size: 0.9em;
+  color: #fffbfa;
+}
+
+.task_container .task_description span {
+  font-size: 0.8em;
+  color: #fffbfac4;
+}
+
+.task_container .delete_task_btn {
+  width: 10%;
+  height: 50%;
+  font-size: 1.5em;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fffbfa;
 }
 </style>
